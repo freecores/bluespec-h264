0b1505d5fe2388da4d5106ef7a5ac37722770608
jamey.hicks <jamey.hicks@d64610c2-7e26-4425-a2f9-44081202e4b6>
2008-06-26 18:01:12 +0000

    fixed reset issue
    git-svn-id: svn+ssh://csg.csail.mit.edu/afs/csail.mit.edu/group/csg/svnroots/blue-projects/trunk/h264@1353 9e70a9ba-87e6-0310-8a3d-bda4c4b0334c
    
    
    Author: kfleming <kfleming@9e70a9ba-87e6-0310-8a3d-bda4c4b0334c>

--- src_verilog/top.v
+++ src_verilog/top.v
@@ -3,17 +3,27 @@ module top;
   reg clk   = 0;
   reg reset = 0;
   
-   always #5 clk <= ~clk; // this corresponds to 10ns duty cycle?
+   always #5 clk = ~clk; // this corresponds to 10ns duty cycle?
 
   mkTH th( .CLK(clk), .RST_N(reset) );
   
   initial
   begin
     // This turns on VCD (plus) output
-    $vcdpluson(0);
-     clk <= 0;    
-     #600000;
-     $finish;
-  end
-    
-endmodule
+    $sdf_annotate("./top.sdf",th.h264);
+    $dumpfile("dump.vcd");
+    $dumpvars;
+     $dumpoff;
+  
+     clk = 0;
+     #30;
+     reset = 1;
+     #7200;
+     $dumpon;
+     #60000;
+     $dumpoff;
+     #1000;
+     $finish; 
+  end 
+     
+endmodule 
