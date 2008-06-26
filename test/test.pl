1bbc797525000a93ced6072fb0a5a7e9fa933da8
jamey.hicks <jamey.hicks@d64610c2-7e26-4425-a2f9-44081202e4b6>
2008-06-26 18:01:30 +0000

    Pipe to file was filling up the harddisk, which is somewhat unreasonable
    git-svn-id: svn+ssh://csg.csail.mit.edu/afs/csail.mit.edu/group/csg/svnroots/blue-projects/trunk/h264@1365 9e70a9ba-87e6-0310-8a3d-bda4c4b0334c
    
    
    Author: kfleming <kfleming@9e70a9ba-87e6-0310-8a3d-bda4c4b0334c>

--- test/test.pl
+++ test/test.pl
@@ -22,7 +22,7 @@ foreach(@h264files)
   `cp ./h264/$_  input.264`;
   system("wc input.264 | awk \'{printf(\"%08x\\n%08x\\n%08x\\n%08x\\n\", \$3, \$3, \$3, \$3)}\' > input_size.hex");
   `perl hexfilegen.pl input.264`;
-  system("./a.out | tee out.txt | grep \"OUT\" | awk \'{print \$2}\' >  out.hex");
+  system("./a.out | grep \"OUT\" | awk \'{print \$2}\' >  out.hex");
   `perl dehex.pl out.hex out_hw.yuv`;
   `./decoder/bin/ldecod.exe -i input.264 -o out_gold.yuv`;
   $out=`diff -q out_gold.yuv out_hw.yuv`;
